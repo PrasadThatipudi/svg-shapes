9234900237af346ad9aad50d00ec06075912404e
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-05-19 00:42:18 +0530

    Adds repeated infinity animation

--- script.js
+++ script.js
@@ -19,25 +19,65 @@ const generatePoints = (
   y,
   n,
   threshold,
-  pointsGenerator = hypo,
+  lineGenerator = hypo,
   points = `${x} ${y}`
 ) => {
   if (n <= 0) {
     return points;
   }
 
-  const { points: newPoints, newX, newY } = pointsGenerator(x, y, threshold);
+  const { points: newPoints, newX, newY } = lineGenerator(x, y, threshold);
 
   return generatePoints(
     newX,
     newY,
     n - 1,
     threshold,
-    pointsGenerator,
+    lineGenerator,
     points.concat(",", newPoints)
   );
 };
 
-const main = () => {};
+const polyLine = (points, fill = "black") =>
+  `<polyline points="${points}" fill="${fill}"></polyline>`;
 
-document.addEventListener("DOMContentLoaded", main);
+const appendToRoot = (data) => {
+  const root = document.querySelector(".board");
+
+  root.innerHTML = data;
+};
+
+function* repeatedCycle(start, n, threshold, sign = 1) {
+  let next = start;
+  let count = 0;
+
+  while (true) {
+    if (count === n) {
+      sign *= -1;
+      count = 0;
+    }
+    yield next;
+    next += threshold * sign;
+    count += 1;
+  }
+}
+
+const animate = () => {
+  const cycle1 = repeatedCycle(50, 5, 10, -1);
+  const cycle2 = repeatedCycle(0, 5, 10);
+
+  setInterval(() => {
+    const start1 = cycle1.next().value;
+    const start2 = cycle2.next().value;
+
+    const line1 = generatePoints(start1, start1, 10, 10, hypo);
+    const line2 = generatePoints(start2, start2, 10, 10, sideOnBase);
+
+    const svg = polyLine(line1, "red").concat(polyLine(line2, "green"));
+    const svgCode = `<svg xmlns="http://www.w3.org/2000/svg">${svg}</svg>`;
+
+    appendToRoot(svgCode);
+  }, 1000);
+};
+
+document.addEventListener("DOMContentLoaded", animate);
